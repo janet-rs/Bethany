74518e8412acc215ac0f70f89f9b33f8606d33ab
alexsoriamqz <alexsoriamqz@gmail.com>
2022-04-21 15:54:51 -0500

    cambios 21 de abril captcha

--- modelo/validacion.php
+++ modelo/validacion.php
@@ -3,6 +3,32 @@
 
     session_start();
 
+    /*if(!empty($_POST)){
+        $nombre = $_POST['usuariolg'];
+        $contraseña = $_POST['passwordlg'];
+        $captcha = $_POST['g-recaptcha-response'];
+
+        $secret = '6Lf8VY8fAAAAACIBtBx5e-Jf-MZWoEvUrtwScKh9';
+
+        if(!$captcha){
+            echo "Por favor verifica el captcha";
+        }
+
+        $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$captcha");
+
+        var_dump($response);
+
+        $arr = json_decode($response, true);
+
+        if($arr['success']){
+            echo '<h3>Thanks </h3>';
+
+        }else{
+            echo '<h3>Error al comprobar Captcha </h3>';
+        }
+
+    }*/
+
     if(isset($_GET['cerrarSesion'])){
         session_unset();
 
@@ -11,29 +37,50 @@
 
     if(isset($_POST['usuariolg']) && isset($_POST['passwordlg'])){
 
-        $sql = "SELECT * FROM usuarios WHERE usuario = '".$_POST['usuariolg']."' AND password = '".$_POST['passwordlg']."'";
-        $resultado = mysqli_query($con,$sql) or die ('Error en el query database');
+        $captcha = $_POST['g-recaptcha-response'];
 
-        $row = mysqli_fetch_array($resultado);
-        if($row == true){
-            //validar rol
-            $rol = $row[4]; 
-            $_SESSION['rol'] = $rol;
+        $secret = '6Lf8VY8fAAAAACIBtBx5e-Jf-MZWoEvUrtwScKh9';
 
-            switch($_SESSION['rol']){
-                case 1:
-                    header("location:../admin.php");
-                break;
-    
-                case 2:
-                    header("location:../usuario.php");
-                break;
-                default:
+        if(!$captcha){
+            echo "Por favor verifica el captcha";
+        }
+
+        $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$captcha");
+
+        var_dump($response);
+
+        $arr = json_decode($response, true);
+
+        if($arr['success']){
+            $sql = "SELECT * FROM usuarios WHERE usuario = '".$_POST['usuariolg']."' AND password = '".$_POST['passwordlg']."'";
+            $resultado = mysqli_query($con,$sql) or die ('Error en el query database');
+
+            $row = mysqli_fetch_array($resultado);
+            if($row == true){
+                //validar rol
+                $rol = $row[4]; 
+                $_SESSION['rol'] = $rol;
+
+                switch($_SESSION['rol']){
+                    case 1:
+                        header("location:../admin.php");
+                    break;
+        
+                    case 2:
+                        header("location:../usuario.php");
+                    break;
+                    default:
+                }
+            }else{
+                //no existe el usuario
+                header('Refresh: 0; http://localhost/Bethany/inicioSesion.php');
+                echo "<script>alert('El usuario o contraseña son incorrectos');</script>";
             }
+
         }else{
-            //no existe el usuario
             header('Refresh: 0; http://localhost/Bethany/inicioSesion.php');
-            echo "<script>alert('El usuario o contraseña son incorrectos');</script>";
+            echo "<script>alert('Error al comprobar Captcha');</script>";
+            //echo '<h3>Error al comprobar Captcha </h3>';
         }
 
     }
--- recupera.php
+++ recupera.php
@@ -1,18 +1,25 @@
 <?php
-    session_start();
 
-    if(isset($_SESSION['rol'])){
-      switch($_SESSION['rol']){
-          case 1:
-              header("location:admin.php");
-          break;
+	require './modelo/conexion.php';
+	require './modelo/funcs.php';
 
-          case 2:
-              header("location:usuario.php");
-          break;
-          default:
-      }
-  }
+	$errors = array();
+
+	if(!empty($_POST)){
+		$email = $mysqli->real_escape_string($_POST['email']);
+	
+		if(!isEmail($email)){
+
+			$errors[] = "Debe ingresar un correo electronico valido";
+			if(emailExiste($email)){
+
+				$user_id = getValor('id', 'correo', $email);
+				$nombre = getValor('nombre', 'correo', $email);
+
+				$token = generaTokenPass($email);
+			}
+		}
+	}
 
 ?>
 
@@ -51,17 +58,6 @@
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-
-  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-
-  <script type="text/javascript">
-      var onloadCallback = function() {
-        grecaptcha.render('html_element', {
-          'sitekey' : '6Lf8VY8fAAAAAKqY_755QBsXvfJY7i38_8iTRRmo'
-        });
-      };
-  </script>
-
 </head>
 
 <body>
@@ -100,64 +96,36 @@
     
   
   <br><br><br><br><br><br><br>
-  <div class="container">
-        <div class="row gy-4">
-			<div class="col-md-6 col-md-offset-3">
-				<div class="panel panel-login">
+  <div class="container">    
+			<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
+				<div class="panel panel-info" >
 					<div class="panel-heading">
-						<div class="row">
-							<h1>Iniciar Sesion</h1>
-						</div>
-						<hr>
-					</div>
-                    <div class="error" style="display:none;">
-                        <span>Datos nos validos, intentelo de nuevo</span>
-                    </div>
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-lg-12">
-
-								<form action="modelo/validacion.php" method="post">
-									<div class="form-group">
-										<input type="text" name="usuariolg" id="usuariolg" tabindex="1" class="form-control" placeholder="Usuario" value="">
-									</div>
-									<div class="form-group">
-										<input type="password" name="passwordlg" id="passwordlg" tabindex="2" class="form-control" placeholder="Contraseña">
-									</div><br>
-                  <div class="g-recaptcha" name="g-recaptcha" data-sitekey="6Lf8VY8fAAAAAKqY_755QBsXvfJY7i38_8iTRRmo"></div>
-									<br>
-                  <div class="form-group">
-										<div class="row text-center">
-											<div class="col-sm-6 col-sm-offset-3">
-												<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="botonlg" value="Iniciar sesión">
-											</div>
-										</div>
-									</div>
-                  
-								</form>
-
-                <div class="form-group">
-								    <div class="row text-center">
-									    <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
-										  <a href="recupera.php">¿Has olvidado la contraseña?</a>
-									    </div>
-								    </div>
-							  </div>
-                <div class="form-group">
-								    <div class="row text-center">
-                        <a href="registro.html">Registrarme</a>
-								    </div>
-							  </div>
-
-                
-
+						<div class="panel-title">Recuperar Contraseña</div>
+						<div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="inicioSesion.php">Iniciar Sesi&oacute;n</a></div>
+					</div>     
+					
+					<div style="padding-top:30px" class="panel-body" >
+						
+						<div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+						
+						<form id="loginform" class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" autocomplete="off">
+							
+							<div style="margin-bottom: 25px" class="input-group">
+								<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+								<input id="email" type="email" class="form-control" name="email" placeholder="email" required>                                        
 							</div>
-						</div>
-					</div>
-				</div>
+							
+							<div style="margin-top:10px" class="form-group">
+								<div class="col-sm-12 controls">
+									<button id="btn-login" type="submit" class="btn btn-success">Enviar</a>
+								</div>
+							</div>
+					   
+						</form>
+					</div>                     
+				</div>  
 			</div>
 		</div>
-	</div>
 
   </main><!-- End #main -->
 
@@ -212,11 +180,7 @@
 
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
-  <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
-        async defer>
-  </script>
 
 </body>
 
 </html>
-
